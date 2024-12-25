508bcdc442aba2cc43d2d766507191db37e14fd3
Sean Li <sean.saifahn.li@gmail.com>
2024-12-25 16:13:40 +0900

    wip: add some kind of colour for chosen cards

--- client/src/lib/board.svelte
+++ client/src/lib/board.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import type { BoardSpace } from '../../../shared/types';
   import { gameState, guessCard } from './gameState.svelte';
   import TeamLogo from './teamLogo.svelte';
   let { spymasterView = false } = $props();
@@ -6,12 +7,22 @@
   const showGuessCardButton = $derived(
     gameState.game?.status === 'inProgress' && gameState.game?.clue.word && !spymasterView
   );
+
+  function borderBasedOnCard(card: BoardSpace) {
+    if (card.flipped) {
+      if (card.identity === 'assassin') return 'border-slate-900 border-2';
+      if (card.identity === 'neutral') return 'border-stone-500 border-2';
+      if (card.identity === 'mirran') return 'border-sky-500 border-2';
+      if (card.identity === 'phyrexian') return 'border-rose-500 border-2';
+    }
+    return 'border-slate-200 hover:border-slate-400';
+  }
 </script>
 
 <div class="grid grid-cols-5 gap-2">
   {#each gameState.game!.board as row, rowIndex}
     {#each row as card, colIndex}
-      <div class="rounded-lg border border-slate-200 p-8 hover:border-slate-400">
+      <div class="rounded-lg border p-4 {borderBasedOnCard(card)}">
         <h4 class="mb-2 font-semibold">{card.word}</h4>
         {#if card.flipped || spymasterView}
           <span class="flex w-12">
