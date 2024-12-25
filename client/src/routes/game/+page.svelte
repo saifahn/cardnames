8efa0a0f0efa2e1af8b78063bd4ccca3226ab10f
Sean Li <sean.saifahn.li@gmail.com>
2024-12-26 01:05:18 +0900

    feat: add link to home page with error

--- client/src/routes/game/+page.svelte
+++ client/src/routes/game/+page.svelte
@@ -2,12 +2,12 @@
   import Board from '$lib/board.svelte';
   import GameInfoSection from '$lib/gameInfoSection.svelte';
   import GameReadyScreen from '$lib/gameReadyScreen.svelte';
-  import { gameState, passTurn } from '$lib/gameState.svelte';
-  import { detailsHaveClue } from '../../../../shared/types';
+  import { gameState } from '$lib/gameState.svelte';
 </script>
 
 {#if gameState.game === null}
   <p>Something went wrong - you shouldn't be here</p>
+  <a href="/" class="mt-2 text-indigo-500 underline dark:text-indigo-300">Go to the home page</a>
 {:else if gameState.game.details.status === 'gameReady'}
   <GameReadyScreen />
 {:else}
--- client/src/routes/game/spymaster/+page.svelte
+++ client/src/routes/game/spymaster/+page.svelte
@@ -22,6 +22,7 @@
 
 {#if gameState.game === null}
   <p>Something went wrong - you shouldn't be here</p>
+  <a href="/" class="mt-2 text-indigo-500 underline dark:text-indigo-300">Go to the home page</a>
 {:else if gameState.game.details.status === 'gameReady'}
   <GameReadyScreen spymasterView={true} />
 {:else}
